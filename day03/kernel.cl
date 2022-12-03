3814d4a0011337b3e71a3c60c51ee1ecd2fe6006
Robbe Van Herck <robbe@robbevanherck.be>
2022-12-03 11:35:01 +0100

    [Day 03] Or maybe it will...

--- day03/kernel.cl
+++ day03/kernel.cl
@@ -33,17 +33,39 @@ __kernel void part1(
             }
         }
     }
-
-    barrier(CLK_GLOBAL_MEM_FENCE);
-
-    if (inp[i] != 0) {
-
-    }
 }
 
 __kernel void part2(
-    __global const long* inp,
-    __global long* out
+    global const int* inp,
+    global const int* lens,
+    global int* out,
+    global int* tmp,
+    global int* tmp2
 ) {
-    out[0] = 1338;
+    int letter = get_global_id(0);
+    int backpack = get_global_id(1);
+    int i = backpack * get_global_size(0) + letter;
+    int group = backpack / 3;
+    int group_i = backpack % 3;
+
+    private int letter_i;
+
+    if (inp[i] > 'Z') {
+        letter_i = inp[i] - 'a';
+    } else {
+        letter_i = inp[i] - 'A' + 26;
+    }
+
+    if (inp[i] != 0) {
+        if (letter < lens[backpack]) {
+            atomic_or(&tmp[group*52 + letter_i], 1 << group_i);
+
+            if (
+                (tmp[group * 52 + letter_i] == 7) &&
+                (atomic_cmpxchg(&tmp2[group * 52 + letter_i], 0, 1) == 0)
+            ) {
+                atomic_add(&out[0], letter_i + 1);
+            }
+        }
+    }
 }
---
+++ day03/part2.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pyopencl as cl
+
+import sys
+
+np.set_printoptions(linewidth=200)
+
+ctx = cl.create_some_context()
+queue = cl.CommandQueue(ctx)
+
+# 1. Read input
+inp = []
+lens = []
+for line in sys.stdin:
+    line = line.strip()
+    inp.append([ord(x) for x in line])
+    lens.append(len(line))
+
+inp = [x + [0] * (52 - len(x)) for x in inp]
+
+# 2. Prepare numpy arrays
+inp_np = np.array(inp, dtype="int32")
+lens_np = np.array(lens, dtype="int32")
+out_np = np.zeros((1,), dtype="int32")
+tmp1_np = np.zeros((len(inp) // 3, 52), dtype="int32")
+tmp2_np = np.zeros((len(inp) // 3, 52), dtype="int32")
+
+# 3. Prepare buffers
+mf = cl.mem_flags
+inp_buffer = cl.Buffer(
+    ctx,
+    mf.READ_ONLY | mf.COPY_HOST_PTR,
+    hostbuf=inp_np
+)
+lens_buffer = cl.Buffer(
+    ctx,
+    mf.READ_ONLY | mf.COPY_HOST_PTR,
+    hostbuf=lens_np
+)
+out_buffer = cl.Buffer(
+    ctx,
+    mf.COPY_HOST_PTR,
+    hostbuf=out_np
+)
+tmp1_buffer = cl.Buffer(
+    ctx, mf.COPY_HOST_PTR,
+    hostbuf=tmp1_np
+)
+tmp2_buffer = cl.Buffer(
+    ctx, mf.COPY_HOST_PTR,
+    hostbuf=tmp2_np
+)
+
+# 4. Create kernel
+with open("kernel.cl", "r") as kernel_file:
+    prg = cl.Program(ctx, kernel_file.read()).build()
+
+# 5. Run kernel
+knl = prg.part2
+
+knl(
+    queue,           # queue
+    (52, len(inp)),   # global shape
+    (1, 1),          # local shape
+    inp_buffer,      # args...
+    lens_buffer,
+    out_buffer,
+    tmp1_buffer,
+    tmp2_buffer,
+)
+
+# 6. Get result
+cl.enqueue_copy(
+    queue,
+    out_np,
+    out_buffer
+)
+print(out_np)
